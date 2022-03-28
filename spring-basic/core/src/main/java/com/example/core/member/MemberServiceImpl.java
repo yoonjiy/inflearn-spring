84e4b8e315c5f2bc9819f8f1933a1588d4a9479f
YOONJI KIM <stritegdc@gmail.com>
2022-03-29 00:00:05 +0900

    [test]: 회원 도메인 테스트

--- spring-basic/core/src/main/java/com/example/core/member/MemberServiceImpl.java
+++ spring-basic/core/src/main/java/com/example/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //구체화에도 의존하고 있음. DIP 위반
 
     @Override
     public void join(Member member) {
