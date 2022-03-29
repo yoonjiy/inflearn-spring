1bbd1de41f814c038778ee5da80c505b258e6e46
YOONJI KIM <stritegdc@gmail.com>
2022-03-30 01:07:11 +0900

    [feat]: AppConfig를 통해 관심사 분리, DIP 준수

--- spring-basic/core/src/test/java/com/example/core/order/OrderServiceTest.java
+++ spring-basic/core/src/test/java/com/example/core/order/OrderServiceTest.java
@@ -1,15 +1,26 @@
 package com.example.core.order;
 
+import com.example.core.AppConfig;
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+   // MemberService memberService = new MemberServiceImpl();
+   // OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
