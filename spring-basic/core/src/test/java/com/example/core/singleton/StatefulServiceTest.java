3cd65fe8d8b91462b484088f388890391534a605
YOONJI KIM <stritegdc@gmail.com>
2022-04-18 22:35:35 +0900

    [test]: 싱글톤 방식 문제점 테스트

---
+++ spring-basic/core/src/test/java/com/example/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.plaf.nimbus.State;
+import java.lang.annotation.Annotation;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        //ThreadA : A 사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+
+        //ThreadB : B 사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        //ThreadA : 사용자A 주문 금액 조회
+        //int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+        //assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}
