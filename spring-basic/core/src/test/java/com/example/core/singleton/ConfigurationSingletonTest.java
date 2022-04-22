44e4a6f240bf33079067debabc1c1f643d3fa63a
YOONJI KIM <stritegdc@gmail.com>
2022-04-22 21:05:23 +0900

    [test]: MemberRepository가 한번만 호출됨을 확인(싱글톤)

---
+++ spring-basic/core/src/test/java/com/example/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,30 @@
+package com.example.core.singleton;
+
+import com.example.core.AppConfig;
+import com.example.core.member.MemberRepository;
+import com.example.core.member.MemberServiceImpl;
+import com.example.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void ConfigurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
