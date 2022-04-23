eceaa15ec1be1c49a3e2b05be8434c9aee43b228
YOONJI KIM <stritegdc@gmail.com>
2022-04-23 23:32:10 +0900

    [test]: componentscan 대상 지정, filtering

---
+++ spring-basic/core/src/test/java/com/example/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package com.example.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
