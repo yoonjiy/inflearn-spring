99e5e11672a293d158e78f26395355c306e61f72
YOONJI KIM <stritegdc@gmail.com>
2022-06-16 22:56:50 +0900

    [refactor]: ObjectProvider 이용, request scope 예제

--- spring-basic/core/src/main/java/com/example/core/web/LogDemoController.java
+++ spring-basic/core/src/main/java/com/example/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.example.core.web;
 
 import com.example.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         myLogger.setRequestURL(requestURL); //스프링 인터셉터로 공통부분 리팩토링 가능
 
         myLogger.log("controller test");
--- spring-basic/core/src/main/java/com/example/core/web/LogDemoService.java
+++ spring-basic/core/src/main/java/com/example/core/web/LogDemoService.java
@@ -2,15 +2,18 @@ package com.example.core.web;
 
 import com.example.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
     //requestURL과 같은 서비스와 상관없는 웹 정보는 컨트롤러 계층까지만 사용해야 한다.
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         myLogger.log("service id = " + testId);
     }
 }
